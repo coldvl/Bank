64e01ffd439b8de5573db5a98e620d3b9f25cc27
coldvl <volodymyr.holodnik@gmail.com>
2022-12-18 12:43:33 +0200

    added branch with class BankAccount

---
+++ BankAccount.h
@@ -0,0 +1,32 @@
+#ifndef BANKACCOUNT_H
+#define BANKACCOUNT_H
+#include <iostream>
+#include "string.h"
+using namespace std;
+
+class BankAccount
+{
+private:
+    int customerId;
+    string ownerName;
+    float balance;
+    
+public:
+    void createAccount();
+    void topUpBalance();
+    void withdrawCash();
+    void showAccount();
+    BankAccount();
+    ~BankAccount();
+    friend class Bank;
+};
+
+BankAccount::BankAccount()
+{
+}
+
+BankAccount::~BankAccount()
+{
+}
+
+#endif
